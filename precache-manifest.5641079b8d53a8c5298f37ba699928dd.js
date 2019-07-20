de2f440bbc245283480f51cc6535a2179a5e368a
PCMACHERO <pcmacmen@gmail.com>
2019-07-19 21:05:52 -0700

    Updates

---
+++ precache-manifest.5641079b8d53a8c5298f37ba699928dd.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7796979b7caa85ad56991d496134167b",
+    "url": "/psmenu/index.html"
+  },
+  {
+    "revision": "d74c00b9c49d83516eb1",
+    "url": "/psmenu/static/css/2.c516b9e4.chunk.css"
+  },
+  {
+    "revision": "bd43dc9541df853b1bb1",
+    "url": "/psmenu/static/css/main.2ad45ece.chunk.css"
+  },
+  {
+    "revision": "d74c00b9c49d83516eb1",
+    "url": "/psmenu/static/js/2.1351dd5d.chunk.js"
+  },
+  {
+    "revision": "bd43dc9541df853b1bb1",
+    "url": "/psmenu/static/js/main.c0fc0edb.chunk.js"
+  },
+  {
+    "revision": "da18934b12bb61f61630",
+    "url": "/psmenu/static/js/runtime~main.a0166fa7.js"
+  },
+  {
+    "revision": "4d2f20d468d4d4205e1ea368bfb4a8fd",
+    "url": "/psmenu/static/media/SST-Light.4d2f20d4.woff"
+  },
+  {
+    "revision": "9f011ccf289e89287d4e31a55ad2f49f",
+    "url": "/psmenu/static/media/g-bo4.9f011ccf.png"
+  },
+  {
+    "revision": "011c53cccfd64f72f75e0a319a76ab68",
+    "url": "/psmenu/static/media/g-detroit.011c53cc.png"
+  },
+  {
+    "revision": "f3957e8cb8387d9a9ef00a235b14de4f",
+    "url": "/psmenu/static/media/g-ghost.f3957e8c.png"
+  },
+  {
+    "revision": "674c78e89dc5625ead8d57eb58670e44",
+    "url": "/psmenu/static/media/g-live.674c78e8.png"
+  },
+  {
+    "revision": "36a50f4d28eac2fc8991643616a6777d",
+    "url": "/psmenu/static/media/g-new.36a50f4d.png"
+  },
+  {
+    "revision": "196d49cf2f36fc057d444f1a9a0e0bb8",
+    "url": "/psmenu/static/media/g-tv.196d49cf.png"
+  },
+  {
+    "revision": "e716276b232f78ffa41a6d32e5191ad2",
+    "url": "/psmenu/static/media/g-video.e716276b.png"
+  },
+  {
+    "revision": "f1d23b90a0cc3e0ac822cc8f733421fa",
+    "url": "/psmenu/static/media/g-vue.f1d23b90.png"
+  },
+  {
+    "revision": "0a99e324593ee1602412360ce895a603",
+    "url": "/psmenu/static/media/i-psbtns.0a99e324.png"
+  },
+  {
+    "revision": "ed2ebcce7662b9024fac9affd3c849dc",
+    "url": "/psmenu/static/media/i-psplus.ed2ebcce.png"
+  },
+  {
+    "revision": "e61fd13dc70811a5a65c902a6dd104bc",
+    "url": "/psmenu/static/media/i-psstore.e61fd13d.png"
+  },
+  {
+    "revision": "9b62a0b09550cb4f3a5bf0ac8eeffe5b",
+    "url": "/psmenu/static/media/psmenubackground.9b62a0b0.mp4"
+  }
+]);
